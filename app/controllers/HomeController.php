a0e7a364185174d651958419cfa004cf621f0ce7
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-09 01:00:03 +0000

    J'ai termin la configuration de twig

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,9 +9,17 @@ class HomeController
 
     public function index()
     {
-        $data['titel']="home page";
-        $data['contentent']="home page hdhdh uzeaufiytz jvcytqfd  ukefzit ";
-       View::load("/Apropos",$data);
+    //     $data['titel']="home page";
+    //     $data['contentent']="home page hdhdh uzeaufiytz jvcytqfd  ukefzit ";
+    //    View::load("/Apropos",$data);
+       // Charger Twig depuis la configuration
+       $twig = require_once dirname( __DIR__) .'/config/Twig.php';
+
+       // Rendre la vue home.html.twig et passer des variables
+       echo $twig->render('Apropos.html.twig', [
+           'variable1' => 'Valeur 1',
+           'variable2' => 'Valeur 2',
+       ]);
 
     }
 }
