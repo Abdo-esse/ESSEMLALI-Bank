75d8b7589fc73c730dba4e83d8799fec42f8a094
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-09 10:33:03 +0000

    J'ai deplacer les pages de landign page a deocier pages

--- app/router/web.php
+++ app/router/web.php
@@ -2,6 +2,6 @@
 
 use App\core\Router;
 
-Router::add("GET","/","HomeController@index");
-Router::add("GET","/Apropos","HomeController@apropos");
-Router::add("GET","/prets","HomeController@prets");
+Router::add("GET","/","PagesController@index");
+Router::add("GET","/Apropos","PagesController@apropos");
+Router::add("GET","/prets","PagesController@prets");
