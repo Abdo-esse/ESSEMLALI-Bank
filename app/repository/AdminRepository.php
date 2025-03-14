da29b5dd60bb543f1f74b7ad453a98fb1ff58fc5
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-14 14:17:33 +0000

    J'ai etrmin la function add admin

--- app/repository/AdminRepository.php
+++ app/repository/AdminRepository.php
@@ -6,43 +6,34 @@ use PDO;
 class AdminRepository  extends BaseRepository
 {
 
+    private $table;
+    private $tablePivot;
+    
     public function __construct() {
         parent::__construct(); 
+        $this->table='';
+        $this->tablePivot='';
     }
     
-    public function addAmin(){
-
+    public function addAmin($data ){
+        
+        $this->conn->beginTransaction(); 
+        try {
+            $AdminId = $this->createAction($this->table, $data);
+            if (empty($AdminId)) {
+                throw new PDOException("Erreur lors de l'insertion d'admin."); 
+            }
+            if (!$this->createAction($this->tablePivot,["user_id"=>$AdminId,"role_id"=>1])) {
+                throw new PDOException("Erreur lors de l'insertion du role ."); 
+            }
+            $this->conn->commit(); 
+        } catch (PDOException $e) {
+            $this->conn->rollBack(); 
+            echo "Erreur : " . $e->getMessage();
+        }
     }
 
-    public function updateCoursTags()
-    {
-        $conn->beginTransaction(); 
 
-try {
-    
-    $sqlDelete = "DELETE FROM cours_tags WHERE idCours = ?";
-    $stmt = $conn->prepare($sqlDelete);
-    if (!$stmt->execute([$this->id])) {
-        throw new PDOException("Erreur lors de la suppression des tags."); 
-    }
-
-  
-    foreach($this->idTags as $idTag )
-    {
-    $sqlInsert = "INSERT INTO cours_tags (idCours, idTags) VALUES (?, ?)";
-    $stmt = $conn->prepare($sqlInsert);
-    if (!$stmt->execute([$this->id, $idTag])) {
-        throw new PDOException("Erreur lors de l'insertion des nouveaux tags."); 
-    }
-    }
-    $conn->commit(); 
-} catch (PDOException $e) {
-    $conn->rollBack(); 
-    echo "Erreur : " . $e->getMessage();
-}
-
-
-    }
 
    
 
