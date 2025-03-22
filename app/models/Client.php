47f7237323eec005207624f321d230da408c5f45
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-03-22 11:12:18 +0000

    J'ai termin le modele cient

---
+++ app/models/Client.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+
+class Client extends User
+{
+    private $sexe;
+    private $telephone;
+    private $adresse;
+    private $carteIdentite;
+    
+    public function __construct($id, $nom, $prenom, $email, $motDePasse, $dateCreation, $isActive, $sexe, $telephone, $adresse, $carteIdentite)
+    {
+        parent::__construct($id, $nom, $prenom, $email, $motDePasse, $dateCreation, $isActive);
+        $this->sexe = $sexe;
+        $this->telephone = $telephone;
+        $this->adresse = $adresse;
+        $this->carteIdentite = $carteIdentite;
+    }
+
+    
+    public function getSexe(){return $this->sexe;}
+    public function getTelephone(){return $this->telephone;}
+    public function getAdresse(){return $this->adresse;}
+    public function getCarteIdentite(){return $this->carteIdentite;}
+
+
+    public function setSexe($sexe){$this->sexe = $sexe;}
+    public function setTelephone($telephone){$this->telephone = $telephone;}
+    public function setAdresse($adresse){$this->adresse = $adresse;}
+    public function setCarteIdentite($carteIdentite){ $this->carteIdentite = $carteIdentite; }
+}
