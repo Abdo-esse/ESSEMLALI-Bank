530a824937d49a31708acd604925bcb838194cc7
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-02 18:48:37 +0000

    termin la function rede client

--- app/config/Twig.php
+++ app/config/Twig.php
@@ -1,13 +1,22 @@
 <?php
 require dirname(__DIR__) . '/../vendor/autoload.php'; 
 
-
 use \Twig\Environment;
 use \Twig\Loader\FilesystemLoader;
+use \Twig\TwigFunction;
 
+// Create a new Twig loader and environment
 $loader = new FilesystemLoader('../app/views'); 
 $twig = new Environment($loader, [
     'cache' => false, 
 ]);
 
+// Register the asset() function
+$twig->addFunction(new \Twig\TwigFunction('asset', function ($path) {
+    // Ensure the correct path from the public folder
+    return '/uploads/' . ltrim($path, '/');
+}));
+
+
+// Return the configured Twig instance
 return $twig;
