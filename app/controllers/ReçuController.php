07e56cd07b071caf2fe585aade566d30350ddd67
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-17 14:50:00 +0100

    termin la telechargement des recu

--- app/controllers/ReçuController.php
+++ app/controllers/ReçuController.php
@@ -22,24 +22,19 @@ class ReçuController extends Controller{
         echo  $this->twig->render('reçu/virement.twig',['session' => $_SESSION ]);
         exit;
     }
+    
     public function telechargerRecuVirement(){
         $data = $_SESSION['data'] ?? null;
-        
         if (!$data) {
-            // Rediriger ou afficher une erreur
             echo "Aucune donnée pour générer le reçu.";
             exit;
-        }
-    
+        }    
         $html = $this->twig->render('reçu/recu_virement.twig', ['session' => $_SESSION]);
-    
         $dompdf = new Dompdf();
         $dompdf->loadHtml($html);
         $dompdf->setPaper('A4', 'portrait');
         $dompdf->render();
         $dompdf->stream('recu_virement.pdf', ["Attachment" => true]);
-        echo"3la molana";
-        exit;
     }
 
     
