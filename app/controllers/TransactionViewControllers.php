b10866b09e8d5c2730d6232cc66bc94a81d97dfd
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-18 10:48:21 +0100

    add TransactionViewControllers pour psr

---
+++ app/controllers/TransactionViewControllers.php
@@ -0,0 +1,30 @@
+<?php 
+namespace App\Controllers;
+
+use App\core\Session;
+
+class TransactionViewControllers extends Controller
+{
+    
+    public function versement()
+    {
+        Session::unset('data');  
+        Session::unset('post');
+       echo  $this->twig->render('employe/versement.twig',['session' => $_SESSION ]);
+    }
+    public function retrait()
+    {
+        Session::unset('data');  
+        Session::unset('post');
+       echo  $this->twig->render('employe/retrait.twig',['session' => $_SESSION ]);
+    }
+    public function virement()
+    {
+        Session::unset('data');  
+        Session::unset('post');    
+       echo  $this->twig->render('employe/virement.twig',['session' => $_SESSION ]);
+    }
+    
+
+
+}
