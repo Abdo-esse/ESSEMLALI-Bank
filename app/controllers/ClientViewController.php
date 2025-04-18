69d1bc8c3083dbad7c5a8382aedf209581673489
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-18 11:38:35 +0100

    add ClientViewController pour psr

---
+++ app/controllers/ClientViewController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controllers;
+
+use App\requests\SignInRequest;
+use App\requests\UpdateClientRequest;
+use App\services\ClientService;
+use App\services\CompteService;
+use App\core\Session;
+class ClientViewController extends Controller
+{
+    private ClientService $clientService;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->clientService=new ClientService();
+    }
+    
+
+    public function index()
+    {
+       echo  $this->twig->render('client/index.twig',['session' => $_SESSION ]);
+    }
+
+    public function edite($id)
+    {
+        $client=$this->clientService->getClient($id);
+        echo  $this->twig->render('client/updateinfo.twig',['session' => $_SESSION,'client' => $client]);
+    }
+   
+    
+
+    public function create()
+    {
+       echo  $this->twig->render('auth/signIn.twig', ['session' => $_SESSION]);
+    }   
+
+ 
+    public function demandeComptes(){
+        $clients=$this->clientService->getAll();
+        echo  $this->twig->render('employe/demandeComptes.twig', ['clients' => $clients]);
+    }
+    public function demandeCompte($id){
+        $client=$this->clientService->find($id);
+        echo  $this->twig->render('employe/demandeCompte.twig', ['client' => $client,]);
+    }
+    public function clients(){
+        $clients=$this->clientService->allClients();
+        echo  $this->twig->render('employe/clients.twig', ['clients' => $clients,]);
+    }
+    public function client($id){
+        $client=$this->clientService->getClient($id);
+        echo  $this->twig->render('employe/client.twig', ['client' => $client,]);
+    }
+ 
+
+    
+
+
+
+    
+}
