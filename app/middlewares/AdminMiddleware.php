777577d3d8eef9b5e0895aef1f8566c5f61baa90
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-19 10:52:58 +0100

    Add logic check role admin

--- app/middlewares/AdminMiddleware.php
+++ app/middlewares/AdminMiddleware.php
@@ -7,7 +7,7 @@ class AdminMiddleware {
 
     public function handle() {
         session_start();
-        if (!isset($_SESSION['user'])) {
+        if (!isset($_SESSION['user'])&& $_SESSION['user']["role"]=="Admin" ) {
             header("Location: /ESSEMLALI-Bank/login");
             exit();
         }
