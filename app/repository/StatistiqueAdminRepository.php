3fa82143f7c5db4a3d22624e2177a64a3cb7dec9
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-23 19:44:22 +0100

    termin les methode de statistique de l'admin de pour la partie reposetori

--- app/repository/StatistiqueAdminRepository.php
+++ app/repository/StatistiqueAdminRepository.php
@@ -11,8 +11,7 @@ class StatistiqueAdminRepository  extends BaseRepository
         $sql = "SELECT COUNT(*) AS total_clients 
                 FROM users 
                 JOIN role_user ru ON ru.user_id = users.id
-                WHERE users.is_active = TRUE AND ru.role_id = 3";
-    
+                WHERE users.is_active = TRUE AND ru.role_id = 3";    
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();    
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -36,7 +35,7 @@ class StatistiqueAdminRepository  extends BaseRepository
         $sql = "SELECT COUNT(*) AS total_employer 
                 FROM users 
                 JOIN role_user ru ON ru.user_id = users.id
-                WHERE users.is_active = TRUE AND ru.role_id = 3";
+                WHERE users.is_active = TRUE AND ru.role_id = 2";
     
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();    
@@ -48,7 +47,7 @@ class StatistiqueAdminRepository  extends BaseRepository
                 FROM users 
                 JOIN role_user ru ON ru.user_id = users.id
                 WHERE users.is_active = TRUE 
-                  AND ru.role_id = 3
+                  AND ru.role_id = 2
                   AND EXTRACT(MONTH FROM date_creation) = EXTRACT(MONTH FROM CURRENT_DATE)
                   AND EXTRACT(YEAR FROM date_creation) = EXTRACT(YEAR FROM CURRENT_DATE)";
         
@@ -56,6 +55,20 @@ class StatistiqueAdminRepository  extends BaseRepository
         $stmt->execute();    
         $result = $stmt->fetch(PDO::FETCH_ASSOC);        
         return $result['total_employer_par_mois'] ?? 0; 
+    }
+    public function nbrHistorique(){
+        $sql = "SELECT count(*) as total_historiques from historique";        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();    
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);        
+        return $result['total_historiques'] ?? 0; 
+    }
+    public function nbrComptes(){
+        $sql = "SELECT count(*) as total_comptes from comptes";        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();    
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);        
+        return $result['total_comptes'] ?? 0; 
     }
     
     
