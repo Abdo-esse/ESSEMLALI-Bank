e19fe6bb45da3fa602c86848ef9997d52dfb7da1
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-04-27 18:03:08 +0100

    je fait ssesion flash a la place sission normal

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -30,7 +30,14 @@ class AdminController extends Controller
     public function admin()
     {
         $admins = $this->adminService->getAll();
-        echo $this->twig->render('admin/admins.twig', ['session' => $_SESSION, 'admins' => $admins]);
+
+        echo $this->twig->render('admin/admins.twig',
+         [
+            'session' => $_SESSION,
+            'admins' => $admins,
+            'error'=> Session::getFlash("error"),
+            'values'=> Session::getFlash("values")
+        ]);
 
     }
 
@@ -38,13 +45,11 @@ class AdminController extends Controller
     {
         $request = new StoreUserRequest($_POST);
         if (!$request->validate()) {
-            Session::set('error', $request->getErrors());
-            Session::set('values', $_POST);
+            Session::setFlash('error', $request->getErrors());
+            Session::setFlash('values', $_POST);
             $this->redirect('admins');
             exit;
         }
-        Session::unset('error');
-        Session::unset('values');
         if (!$this->adminService->create($_POST)) {
             Session::set('error', "Une erreur s'est produite lors de l'ajout de l'administrateur.");
             $this->redirect('admins');
